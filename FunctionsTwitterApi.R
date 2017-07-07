4d4d0138456e6cffaf560f055a0450bbc05bd128
Aniqa Rehman <raniqa57@gmail.com>
2017-07-07 18:33:11 +0200

    rPython package added

---
+++ FunctionsTwitterApi.R
@@ -0,0 +1,8 @@
+install.packages("rPython")
+library(rPython)
+
+install.packages("devtools")
+library(devtools)
+
+install.packages("RJSONIO", repos = "http://www.omegahat.org/R", type = "source")
+install("G:/rPython")
