957734debd1cb5ff5c7a1885d399b28d140bfe6b
praulamo <praulamo@gmail.com>
2017-09-06 15:02:52 +0300

    Function interface for fetching data

--- GetStackOverflowData.R
+++ GetStackOverflowData.R
@@ -1,27 +1,41 @@
-setwd('G:/EMSE-Masters/Oulu University - Finland/Internship/project/TrendMining')
-rm(list = ls())
-source("G:/EMSE-Masters/Oulu University - Finland/Internship/project/TrendMining/FunctionsStackOverflowApi.R")
+source("FunctionsStackOverflowApi.R")
 
-query_string = "Robot Framework"
-query_string = "Test Automation"
-query_string = "Selenium"
+#For example
+#app_path => set path to your project folder
+#query_string = "jenkins"
+#my_filename = "jenkins"
 
-my_articles <- get_stackoverflow_data(query_string)
+get_stackOverFlowData = function(app_path, query_string, my_filename) {
 
-abstract <- my_articles$Abstract
-abstract <- gsub("<.*?>", "", abstract)
-abstract <- gsub("[\r\n]", " ", abstract)
-abstract <- gsub("\"", "", abstract)
+  my_articles = get_stackoverflow_data(query_string)
 
-#Add cleaned abstracts as a new column. We could also replace the existing but debugging is easier if we keep both. 
-my_articles$Abstract_clean <- tolower(abstract)
-my_articles$Title <- tolower (my_articles$Title)
+  abstract = my_articles$Abstract
+  abstract = gsub("<code.*/code>", "", abstract)
+  abstract = gsub("<code.*<truncated>", "", abstract)
+  abstract = gsub("<.*?>", "", abstract)
+  abstract = gsub("//.*\n", " ", abstract)
+  abstract = gsub("\\{\n.*\\}\n", " ", abstract)
+  abstract = gsub("[\r\n]", " ", abstract)
+  abstract = gsub("\"", "", abstract)
+  abstract = gsub("[0-9]", "", abstract)
 
-#Date is character covert to Date objec
-my_articles$Date <- as.Date(my_articles$Date)
+  #Add cleaned abstracts as a new column. 
+  #We could also replace the existing but debugging is easier if we keep both. 
+  my_articles$Abstract_clean = tolower(abstract)
+  my_articles$Title = tolower(my_articles$Title)
 
-save(my_articles, file="stackoverflow_articles_clean.RData")
+  #Date is character covert to Date object
+  my_articles$Date = as.Date(my_articles$Date)
+  my_articles$CR_Date = as.Date(my_articles$CR_Date)
+  my_articles$LA_Date = as.Date(my_articles$LA_Date)
 
+  #Fixed filename: data/my_STO_<xxx>_data.RData
+  my_file = app_path
+  my_file = paste(my_file, "data/my_STO_", sep="", collapse=" ")
+  my_file = paste(my_file, my_filename, sep="", collapse=" ")
+  my_file = paste(my_file, "_data.RData", sep="", collapse=" ")
+  
+  save(my_articles, file=my_file)
 
-
-
+  return(my_file)
+}
