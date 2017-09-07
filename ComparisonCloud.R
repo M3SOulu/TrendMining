ca1151deeba8ba72b0246ab36ffd9422470711c8
praulamo <27272908+praulamo@users.noreply.github.com>
2017-09-08 00:34:34 +0300

    Function interface.
    You may use this, e.g.
    > my_file = "my_STO_ci_data.RData"
    > draw_ComparisonCloud(my_file)
    > draw_FourWayComparisonCloud(my_file)

--- ComparisonCloud.R
+++ ComparisonCloud.R
@@ -1,57 +1,110 @@
-rm(list = ls())
-setwd('C:/Users/mmantyla/Dropbox/Teach/2017_Trends')
-setwd('D:/Dropbox/Teach/2017_Trends')
-load ("my_articles_clean.RData")
-
-#Let view a timeline
-summary(my_articles$Date)
-#Remove articles with no date
-my_articles_2 <- my_articles[which(!is.na(my_articles$Date)),]
-#split from the middle NA's couse trouble so we need [which(!is.na(my_articles$Date))]
-new_titles <- my_articles$Title[my_articles$Date > median(my_articles_2$Date)]
-old_titles <- my_articles$Title[my_articles$Date <= median(my_articles_2$Date)]
-#to single 
-all_titles <- c(paste(new_titles, collapse=" "), paste(old_titles, collapse=" "))
+#install.packages("magrittr", dependencies = TRUE)
+#install.packages("tm", dependencies = TRUE)
+#install.packages("wordcloud", dependencies = TRUE)
 library(magrittr)
 library(tm)
 library(wordcloud)
 
-removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]","",x)
-all_titles <- removeSpecialChars(all_titles)
-all_titles <- removeWords(all_titles,c(stopwords("english"), "can", "used", "use", "paper", 
-                                       "approach", "however", "new", "study", "using", "software",
-                                       "introduction"))
-tdm <- all_titles %>% VectorSource %>% Corpus %>% TermDocumentMatrix %>% as.matrix
+my_stopwords = c(stopwords("english"),"able","add","also","although","app",
+                 "apps","application","applications","apply",
+                 "applied","applying","approach","arsenic","author","authors",
+                 "best","based","better","brazil","built","business",
+                 "can","case","class","classes","computer","could",
+                 "company","device","different","doesnt","drug",
+                 "early","entire","even","every",
+                 "file","find","florida","food","get","getting",
+                 "health","height","high","highly","however",
+                 "industry","insertion","insertions","introduction","know",
+                 "large","level","literature","local","making",
+                 "many","may","mice","much","naked","name","needs","new","null",
+                 "often","old","overview","paper","project","projects",
+                 "really","reading","river","run","running",
+                 "searched","seems","set",
+                 "several","similar","since","small","software",
+                 "softwaredevelopment","step","story","student","students","study","studies",
+                 "system","systems","team","teams","text","tell",
+                 "unable","update","updated","updating","usa","use","used","user","users","using",
+                 "versus","visit","vitamin","want","within","without","write","writing",
+                 "year","years","one","two","three","four","five","six","seven","eight")
 
-colnames(tdm) <- c (paste (">", as.character(median(my_articles_2$Date))), 
-                    paste ("<=", as.character(median(my_articles_2$Date))))
+draw_ComparisonCloud = function(my_file){
+
+  my_temp_file = paste(my_data_dir, "/", sep="")
+  my_temp_file = paste(my_temp_file, my_file, sep="")
+  load(my_temp_file)
+
+  print(paste("Creating comparison cloud, my_file:", my_temp_file))
+  
+  #Remove articles with no date
+  my_articles_2 <- my_articles[which(!is.na(my_articles$Date)),]
+
+  #Split from the middle 
+  #(NA's couse trouble so we need [which(!is.na(my_articles$Date))])
+  new_titles <- my_articles$Title[my_articles$Date > median(my_articles_2$Date)]
+  old_titles <- my_articles$Title[my_articles$Date <= median(my_articles_2$Date)]
+
+  #To single 
+  all_titles <- c(paste(new_titles, collapse=" "), paste(old_titles, collapse=" "))
+
+  removeSpecialChars <- function(x) gsub("[^a-zA-Z ]","",x)
+  all_titles <- removeSpecialChars(all_titles)
+  all_titles = removeWords(all_titles, my_stopwords)
+
+  tdm <- all_titles %>% VectorSource %>% Corpus %>% TermDocumentMatrix %>% as.matrix
+
+  colnames(tdm) <- c (paste (">", as.character(median(my_articles_2$Date))), 
+                  paste ("<=", as.character(median(my_articles_2$Date))))
                          
-comparison.cloud(tdm, max.words=100, title.size=1.5)
-rm(my_articles_2)
-#----------------------------------------------
-# How about citations?
-summary(my_articles$Cites)
+  comparison.cloud(tdm, max.words=50, rot.per=0, 
+                   colors=brewer.pal(3,"Set1"),
+                   title.size=1.5)
 
-#This time we do four way split
-q1_t <- my_articles$Title[my_articles$Cites <= quantile(my_articles$Cites, probs = 0.25)]
-q2_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.25) &
-                          my_articles$Cites <= quantile(my_articles$Cites, probs = 0.5)]
-q3_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.5) &
-                          my_articles$Cites <= quantile(my_articles$Cites, probs = 0.75)]
-q4_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.75)]
+  rm(my_articles_2)
+  print("Finished comparison cloud")
+}
 
-all_titles <- c(paste(q1_t, collapse=" "), 
-                paste(q2_t, collapse=" "),
-                paste(q3_t, collapse=" "), 
-                paste(q4_t, collapse=" "))
+draw_FourWayComparisonCloud = function(my_file){
 
-#Start copy-paste
-all_titles <- removeSpecialChars(all_titles)
-all_titles <- removeWords(all_titles,c(stopwords("english"), "can", "used", "use", "paper", 
-                                       "approach", "however", "new", "study", "using", "software",
-                                       "introduction"))
-tdm <- all_titles %>% VectorSource %>% Corpus %>% TermDocumentMatrix %>% as.matrix
-#End copy-paste
-colnames(tdm) <- c ("Q1", "Q2", "Q3", "Q4")
-comparison.cloud(tdm, max.words=80, title.size=1.5)
+  my_temp_file = paste(my_data_dir, "/", sep="")
+  my_temp_file = paste(my_temp_file, my_file, sep="")
+  load(my_temp_file)
 
+  print(paste("Creating 4-Way Comparison cloud, my_file:", my_temp_file))
+    
+  #This time we do four way split
+  q1_t <- my_articles$Title[my_articles$Cites <= quantile(my_articles$Cites, probs = 0.25)]
+  q2_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.25) &
+              my_articles$Cites <= quantile(my_articles$Cites, probs = 0.5)]
+  q3_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.5) &
+              my_articles$Cites <= quantile(my_articles$Cites, probs = 0.75)]
+  q4_t <- my_articles$Title[my_articles$Cites > quantile(my_articles$Cites, probs = 0.75)]
+
+  if(length(q1_t) == 0)
+    q1_t = "no_data"
+  if(length(q2_t) == 0)
+    q2_t = "no_data"
+  if(length(q3_t) == 0)
+    q3_t = "no_data"
+  if(length(q4_t) == 0)
+    q4_t = "no_data"
+  
+  all_titles <- c(paste(q1_t, collapse=" "), 
+          paste(q2_t, collapse=" "),
+          paste(q3_t, collapse=" "), 
+          paste(q4_t, collapse=" "))
+
+  removeSpecialChars <- function(x) gsub("[^a-zA-Z ]","",x)
+  all_titles <- removeSpecialChars(all_titles)
+  all_titles = removeWords(all_titles, my_stopwords)
+  
+  tdm <- all_titles %>% VectorSource %>% Corpus %>% TermDocumentMatrix %>% as.matrix
+
+  colnames(tdm) <- c ("Q1 <= 0.25", "0.25 < Q2 <= 0.5", 
+                      "0.5 < Q3 <= 0.75", "Q4 > 0.75")
+  comparison.cloud(tdm, max.words=50, rot.per=0, title.size=1.5, colors=brewer.pal(4,"Set1"))
+  
+  rm(my_articles)
+  
+  print("FInished 4-Way Comparison cloud")
+  
+}
