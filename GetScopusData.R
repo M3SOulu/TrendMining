5dc4c393b7cdf26a7e6a71fe7a2adbfd94e0d37d
praulamo <27272908+praulamo@users.noreply.github.com>
2017-09-07 11:11:03 +0300

    Clean-up.
    See required environmental variables in Initialize.R
    Usage of function, e.g.
    
    > get_ScopusData("jira", "jira")
    HTTP specified is:http://api.elsevier.com/content/search/scopus
    [1] "start 25  found  -15"
    [1] "K:/My Documents/Projects/TrendMining_2017/TrendMining/data/my_scopus_jira_data.RData"
    >

--- GetScopusData.R
+++ GetScopusData.R
@@ -3,21 +3,20 @@
 library(text2vec)
 library(rscopus)
 
-#Replace Scopus key-line with your OWN Scopus key: set_api_key("YOUR_SCOPUS_KEY_HERE")
 source("FunctionsScopusApi.R")
-source("Set_MyScopus_APIKey.R")
 
 #Fetch scopus articles and a get a dataframe
 #Please see restrictions: #https://dev.elsevier.com/api_key_settings.html
 #Trying the query first via Scopus web-interface is recommended
 
-#CREATE a sub-folder "data" to your project folder (where the project scripts are)
+#query_string = string to be searched
+#my_filename = string to be used as a part of the filename
+
 #For example
-#app_path => set path to your project folder (e.g. use getwd()) - if null, the current work directory will be used
 #query_string = "Continuous Integration"
 #my_filename = "ci"
 
-get_ScopusData = function(app_path, query_string, my_filename){
+get_ScopusData = function(query_string, my_filename){
 
   my_query_string = "TITLE-ABS-KEY(\""
   my_query_string = paste(my_query_string, query_string, sep="")
@@ -25,6 +24,7 @@ get_ScopusData = function(app_path, query_string, my_filename){
   
   #Get articles and save those - we do not want to re-run the query
   my_articles = get_scopus_papers(my_query_string)
+  
   #save(my_articles, file="data/my_SCOPUS_articles_dirty.RData")
   #load ("data/my_SCOPUS_articles_dirty.RData")
 
@@ -52,7 +52,7 @@ get_ScopusData = function(app_path, query_string, my_filename){
   my_articles$Date = as.Date(my_articles$Date)
   
   #Fixed filename: /data/my_scopus_<xxx>_data.RData
-  my_file = ifelse(!is.null(app_path), app_path, getwd())
+  my_file = my_work_dir
   my_file = paste(my_file, "/data/my_scopus_", sep="", collapse=" ")
   my_file = paste(my_file, my_filename, sep="", collapse=" ")
   my_file = paste(my_file, "_data.RData", sep="", collapse=" ")
