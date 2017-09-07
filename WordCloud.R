7da147151f18bcdba8390a37ce02f5f91ecaab17
praulamo <27272908+praulamo@users.noreply.github.com>
2017-09-08 00:37:35 +0300

    Function interface
    You may use this, e.g.
    > my_file = "my_STO_ci_data.RData"
    > draw_myWordCloud(my_file)

--- WordCloud.R
+++ WordCloud.R
@@ -1,31 +1,52 @@
-rm(list = ls())
-#setwd('C:/Users/mmantyla/Dropbox/Teach/2017_Trends')
-#setwd('D:/Dropbox/Teach/2017_Trends')
-load ("my_articles_clean.RData")
-load ("twitter_articles_clean.RData")
-load ("stackoverflow_articles_clean.RData")
-
-
-my_text <- paste (my_articles$Title, my_articles$Abstract_clean)
-
-#library(text2vec)
-library (tm)
+#install.packages("text2vec")
+#install.packages("tm")
+#install.packages("magrittr")
+#install.packages("wordcloud")
+library(text2vec)
+library(tm)
 library(magrittr)
-#This line could also be part of getting data. 
-removeSpecialChars <- function(x) gsub("[^a-zA-Z0-9 ]","",x)
-my_text = my_text %>% removeNumbers %>% removeSpecialChars %>% tolower # = tolower(removePunctuation(removeNumbers(my_text)))
-
-
-#install.packages("wordcloud", dependencies = TRUE)
 library(wordcloud)
-wordcloud(my_text, max.words = 50, random.order = FALSE)
 
-#Since our data was "TITLE-ABS-KEY(\"agile software development process\")"
-#it makes sense to remove the works required in the query and re-plot
-my_text = removeWords(my_text, c ("agile", "software", "development", "process", "processes"))
-
-my_text = removeWords(my_text, c ("robot", "framework", "development", "process", "processes"))
-my_text = removeWords(my_text, c ("test", "automation", "twitter", "http", "com"))
-
-#A good is to remove more words that we do not care about 
-my_text = removeWords(my_text, c ("can", "used", "use", "paper", "approach", "however", "new", "study", "using"))
+draw_myWordCloud = function(my_file){
+  
+  my_temp_file = paste(my_data_dir, "/", sep="")
+  my_temp_file = paste(my_temp_file, my_file, sep="")
+  load(my_temp_file)
+  
+  print(paste("Creating Word cloud, my_file: ", my_file))
+  
+  my_text <- paste(my_articles$Title, my_articles$Abstract_clean)
+  my_text = tolower(my_text)
+  
+  #A good is to remove more words that we do not care about 
+  my_stopwords = c(stopwords("english"),"able","add","also","although","apply",
+                   "app","apps","application","applications","applied","applying",
+                   "approach","author","authors","based","best","business",
+                   "can","case","class","classes","computer","could",
+                   "company","device","different","doesnt","early","entire","every",
+                   "find","file","fine","first","found","following","getting","get",
+                   "health","height","high","higher","highly","however",
+                   "industry","info","insertion","insertions","introduction","just",
+                   "know","large","level","like","literature","local",
+                   "making","many","naked","name","need","needs","new","now","null",
+                   "may","much","often","old","overview","paper","please","project","projects",
+                   "really","run","searched","see","seems","select","set","several",
+                   "similar","since","small","showed","software","softwaredevelopment",
+                   "something","still","story","student","students","study","studies",
+                   "system","systems",
+                   "team","teams","testing","text","tell","thanks","tried","try","trying",
+                   "update","updated","updating","unable","use","used","user","users","using",
+                   "various","versus","want","way","well","will","within",
+                   "write","writing","without",
+                   "year","years","one","two","three","four",
+                   "horses","polo","animals","toxic","henan","keshan",
+                   "toxicosis","high","clinical","province","signs","
+                   disease","death")
+  
+  my_text = removeWords(my_text, my_stopwords)
+  
+  wordcloud(my_text, max.words=50, min.freq=5, random.order=FALSE, rot.per=0)
+  
+  rm(my_text)
+  print("Finished Word cloud")
+}
